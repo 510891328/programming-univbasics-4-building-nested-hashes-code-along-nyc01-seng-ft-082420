61aeab83ae0845bbdf168246065dfd6bc7ebeb0c
Yunhao Gao <as510891328@gmail.com>
2020-08-06 23:09:38 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,7 +10,9 @@ def third_challenge
       :hero => {
         {:name => "Romeo", :age => 15, :status: "alive"}
         },
-      :hero_friends => []
+      :hero_friends => {
+        {}
+      }
    }, 
    :capulet => {
       :patriarch => {
